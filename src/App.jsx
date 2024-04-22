30d4f9fed817ff52d0fb46b1edaa2378868dfcc3
alexyliao <111948810+alexyliao@users.noreply.github.com>
2024-04-22 15:26:06 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
+import React, { useContext } from "react";
+import "./global.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthContext } from "./context/AuthContext";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import VocabGame from "./pages/VocabGame";
-import "./global.css";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { useContext } from "react";
-import { AuthContext, AuthContextProvider } from "./context/AuthContext";
+import MainPage from "./pages/MainPage";
+import VocabGame from "./pages/VocabGame"; 
+
 function App() {
   const { currentUser } = useContext(AuthContext);
 
+  // Component for a protected route
   const ProtectedRoute = ({ children }) => {
     if (!currentUser) {
-      return <Navigate to="/login" />;
+      return <Navigate to="/login" replace />;
     }
     return children;
   };
@@ -19,19 +22,14 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/">
-          <Route
-            index
-            element={
-              <ProtectedRoute>
-                <Home />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route path="vocab-game" element={<ProtectedRoute><VocabGame /></ProtectedRoute>} />
-        </Route>
+        <Route path="/" element={!currentUser ? <Navigate to="/login" replace /> : <Navigate to="/main" replace />} />
+        <Route path="login" element={!currentUser ? <Login /> : <Navigate to="/main" replace />} />
+        <Route path="register" element={!currentUser ? <Register /> : <Navigate to="/main" replace />} />
+        <Route path="main" element={<ProtectedRoute><MainPage /></ProtectedRoute>} />
+        <Route path="home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+        <Route path="vocab-game" element={<ProtectedRoute><VocabGame /></ProtectedRoute>} />
+        {/* Redirects any non-defined route to the main route */}
+        <Route path="*" element={!currentUser ? <Navigate to="/login" replace /> : <Navigate to="/main" replace />} />
       </Routes>
     </BrowserRouter>
   );
